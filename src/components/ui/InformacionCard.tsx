c53d26ecc7e4dc047dce7c3253153b2b99ce6db6
Felipe Zelarayan <felipezelarayan@hotmail.es>
2026-07-28 21:43:28 -0300

    Actualizar version a 4.0.1 y agregar traducciones faltantes en rutinas del alumno

--- src/components/ui/InformacionCard.tsx
+++ src/components/ui/InformacionCard.tsx
@@ -2,7 +2,7 @@
 
 import { useIdioma } from "@/lib/i18n-context";
 
-export const APP_VERSION = "4.0.0";
+export const APP_VERSION = "4.0.1";
 export const LAST_UPDATE = "28/07/2026";
 
 export default function InformacionCard() {
